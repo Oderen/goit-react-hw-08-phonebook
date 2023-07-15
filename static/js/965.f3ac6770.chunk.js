149b8f62b26bab58c9f1096b26d91bad23e7b5b6
Oderen <114490326+Oderen@users.noreply.github.com>
2023-07-15 10:52:55 +0000

    Deploying to gh-pages from @ Oderen/goit-react-hw-08-phonebook@e4229dd293c9df70945abc7e3b4a68da1a7ec481 🚀

--- static/js/965.f3ac6770.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[965],{6965:function(e,t,n){n.r(t);var o=n(184);t.default=function(){return(0,o.jsx)("h1",{style:{marginLeft:30},children:"Home"})}}}]);
-//# sourceMappingURL=965.f3ac6770.chunk.js.map
